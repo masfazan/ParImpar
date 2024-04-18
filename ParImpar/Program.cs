3cf83dfb9f0d7ab0e1dc531b4da3827b46a22ebb
masfazan <msfazan@icloud.com>
2024-04-18 16:03:47 -0300

    Add project files.

---
+++ ParImpar/Program.cs
@@ -0,0 +1,12 @@
+﻿//int numero =int.Parse(Console.ReadLine());
+Console.WriteLine("Digite um numero: ");
+int numero=int.Parse(Console.ReadLine());
+    if (numero % 2 == 0)
+{
+    Console.WriteLine("É par!");
+}
+    else
+{
+    Console.WriteLine("É impar!");
+}
+ Console.ReadLine();
